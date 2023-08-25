634e9c96150969418c1feaa202c21bf2f6dd9245
= <lucas.freire61@aluno.ifce.edu.br>
2023-08-25 18:16:43 -0300

    update api

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -75,6 +75,7 @@ class NotesController{
       .whereLike("notes.title", `%${title}%`)
       .whereIn("name", filterTags)
       .innerJoin("notes", "notes.id", "tags.note_id")
+      .groupBy("note_id")
       .orderBy("notes.title")
 
     } else 
